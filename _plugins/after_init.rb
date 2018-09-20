3e89c49bfff829e3e33a25473fa7c55e52051cf3
Alexis Pojomovsky <apojomovsky@ekumenlabs.com>
2018-09-20 17:24:02 -0300

    WIP

--- _plugins/after_init.rb
+++ _plugins/after_init.rb
@@ -2,11 +2,10 @@
 require 'fileutils'
 
 Jekyll::Hooks.register :site, :after_init do |site|
-    STDOUT.puts "HOLA HOLA HOLA HOLA".green
-    STDOUT.puts "HOLA HOLA HOLA HOLA".green
-    site.config.documentation_repos.each do |repo_dir|
+    site.config['documentation_repos'].each do |repo_dir|
         STDOUT.puts "Path to repo to copy: #{File.join(site.source, "_remotes", repo_dir)}".green
-        #FileUtils.rm_rf(File.join(site.source, "_remotes", repo_dir)
-        #FileUtils.cp_r(repo_dir)
+        STDOUT.puts "Destination path: #{File.join(site.source)}".green
+        FileUtils.rm_rf(File.join(site.source, repo_dir))
+        FileUtils.cp_r(File.join(site.source, "_remotes", repo_dir), File.join(site.source))
     end
 end
